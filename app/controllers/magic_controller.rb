6ed2a144b22c7355d6ca40241cff1410d97cbb59
jeremygradisher <jeremygradisher@gmail.com>
2023-01-31 02:28:27 +0000

    Messing with the Magic-sdk and some testing

--- app/controllers/magic_controller.rb
+++ app/controllers/magic_controller.rb
@@ -1,4 +1,9 @@
 class MagicController < ApplicationController
   def index
+      
+      #Trying to figure out how this works:
+      #@card = MTG::Card.find(386616)
+      #@cards = MTG::Card.where(name: 'Westgate Regent').all
+
   end
 end
