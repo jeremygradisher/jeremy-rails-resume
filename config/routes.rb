42c6b307fec01ab1001fd6726a61fc2f2883fab1
jeremygradisher <jeremygradisher@gmail.com>
2023-03-06 04:54:43 +0000

    Catching up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cards/search'
   resources :areaimages
   resources :areas
   resources :mapimages
@@ -10,6 +11,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get 'magic', to: "magic#index"
+  get '/cards/search', to: 'cards#search'
+  get '/cards/primal_vigor', to: 'cards#show'
+  
   get 'react', to: "react#index"
   
   #resources :users, :only => [:index, :show, :destroy]
